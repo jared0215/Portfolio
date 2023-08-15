95cccb1646c0cbf6c28fb03f211b3481a5fa8ef5
Jared Campos <99213397+jared0215@users.noreply.github.com>
2023-08-15 16:41:52 -0400

    Creat initial protfolio page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,113 +1,125 @@
-import Image from 'next/image'
+import Head from 'next/head';
+import { FaGlobeAmericas, FaCode, FaChild, FaProjectDiagram } from 'react-icons/fa';
+import { Roboto_Mono } from 'next/font/google';
+import Image from 'next/image';
+
+const instagramIcon = require('../public/instagram.svg');
+const linkedinIcon = require('../public/linkedin.svg');
+const jared = require('../public/Jared.png');
+
+const roboto = Roboto_Mono({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700'],
+})
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <Head>
+        <title>Jared Campos Portfolio</title>
+        <meta name='description' content='Home page' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className={'px-10 bg-[#FFFCF2] ${roboto.className} md:px-20 lg:px-40' }>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-[#403D39] text-lg '>Made by Jared Campos</h1>
+            <ul className='flex items-center'>
+              <li>
+                <FaGlobeAmericas className='cursor-pointer text-2xl ' color='#403D39' />
+              </li>
+              <li>
+                <a href="#" className='bg-gradient-to-r from-[#35844F] to-[#4FAB6D] text-white px-4 py-2 rounded ml-8'>Resume</a>
+              </li>
+            </ul>
+          </nav>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className='text-center'>
+            <h2 className='text-[#35844F] text-4xl font-bold mb-4 md:text-6xl'>Hi, I'm Jared Campos</h2>
+            <h4 className='text-[#403D39] text-2xl font-medium mb-4 md:text-4xl'>Software Developer</h4>
+            <p className='text-[#403D39] max-w-3xl mx-auto'>
+              As a recent graduate from
+              Coding Dojo's Software Development program, 
+              I'm embarking on an exciting journey to forge my career.
+              Join me as I delve into what I've learned
+              as I take my first steps in the vibrant world of technology!
+            </p>
+            <div className='flex justify-center my-10'>
+              <Image src={linkedinIcon} alt='linkedin icon' width={50} height={50} className='cursor-pointer' />
+              <Image src={instagramIcon} alt='instagram icon' width={50} height={50} className='cursor-pointer ml-10' />
+            </div>
+            <Image src={jared} alt='jared campos' width={200} height={200} className='cursor-pointer mx-auto relative' />
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        </section>
+        
+        {/* Cards */}
+        <section className='min-h-screen'>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          {/* About Me */}  
+          <div className='lg:flex gap-10'>
+            <div className='shadow-xl my-10 p-10 rounded-3xl flex-1'>
+              <FaChild className='text-[#35844F] text-4xl mx-auto' />
+              <h3 className='text-[#403D39] text-2xl font-bold my-4 text-center'>About me</h3>
+              <p className='text-[#403D39] max-w-3xl'>
+                I'm a software developer with a passion for learning and creating.
+                I'm always looking for new ways to challenge myself and expand my knowledge.
+                I'm a firm believer in the power of technology to change the world for the better.
+                I'm excited to be a part of the future of technology and to be able to contribute to it.
+              </p>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            {/* Technical Skills */}
+            <div className='shadow-xl my-10 p-10 rounded-3xl flex-1'>
+              <FaCode className='text-[#35844F] text-4xl mx-auto' />
+              <h3 className='text-[#403D39] text-2xl font-bold my-4 text-center'>Technical Skills</h3>
+              <div className='grid grid-cols-2 gap-4'>
+                <div className='flex flex-col'>
+                  <ul className='text-[#403D39] text-center'>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Next.js</li>
+                    <li>Bootstrap</li>
+
+                  </ul>
+                </div>
+                <div className='flex flex-col'>
+                  <ul className='text-[#403D39] text-center'>
+                    <li>Python</li>
+                    <li>Django</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* Github Projects */}
+            <div className='shadow-xl my-10 p-10 rounded-3xl flex-1'>
+              <FaProjectDiagram className='text-[#35844F] text-4xl mx-auto' />
+              <h3 className='text-[#403D39] text-2xl font-bold my-4 text-center'>Github Projects</h3>
+              <ul className='text-[#403D39] text-center'>
+                <li>
+                  <a href='#' className='text-[#35844F] hover:text-[#FFB95B]'>Thriftlemore</a>
+                </li>
+                <li>
+                  <a href='#' className='text-[#35844F] hover:text-[#FFB95B]'>Poker Earnings</a>
+                </li>
+                <li>
+                  <a href='#' className='text-[#35844F] hover:text-[#FFB95B]'>Portfolio</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+        </section>
+
+      </main>
+    </div>
   )
 }
